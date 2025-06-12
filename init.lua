f1dca2eaf520306859cebd27b50b79ec8bb680a3
ashish16052 <ashishmohite16052@gmail.com>
2025-06-12 12:21:36 +0530

    add copilot plugin
    - add 'github/copilot.vim' plugin to init.lua
    - run `:Copilot setup`
    
    reference: https://github.com/github/copilot.vim

--- init.lua
+++ init.lua
@@ -991,6 +991,9 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  --
+  -- copilot plugin
+  'github/copilot.vim',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
