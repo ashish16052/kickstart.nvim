5b91ad5b4171bb22ddb3d1521ebb507ff5071655
ashish16052 <ashishmohite16052@gmail.com>
2025-06-20 23:37:18 +0530

    Add TypeScript language server (ts_ls)
    - Adds ts_ls for comprehensive TypeScript and JavaScript language support
    - Provides IntelliSense, type checking, and code navigation for TS/JS files
    - Complements existing ESLint setup for a complete JavaScript development experience

--- init.lua
+++ init.lua
@@ -685,6 +685,8 @@ require('lazy').setup({
         },
         pyright = {},
         angularls = {},
+        -- TypeScript/JavaScript language server
+        ts_ls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
