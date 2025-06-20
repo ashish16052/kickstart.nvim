4495f831f039357a64eb57ec16f207348ff8fc8f
ashish16052 <ashishmohite16052@gmail.com>
2025-06-20 23:38:16 +0530

    Add CSS language server for style development
    - Adds cssls for CSS and SCSS language support
    - Provides IntelliSense, validation, and completion for stylesheet files
    - Complements stylelint setup for comprehensive CSS development workflow

--- init.lua
+++ init.lua
@@ -693,6 +693,8 @@ require('lazy').setup({
             workingDirectory = { mode = "auto" },
           },
         },
+        -- CSS/SCSS language server
+        cssls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
